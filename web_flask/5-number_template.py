3dac76c2853bd99b29245b9fc94dd10d053d51be
Jo2831 <jo2831@github.com>
2022-11-12 00:41:10 -0900

    updated_task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 written by: yoseph-desalegn
-cont:Write a script that starts a Flask web application.
+cont:Write a script that starts a Flask web application
 """
 
 from flask import Flask, render_template
