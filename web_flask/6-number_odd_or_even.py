7c2e4e7af24a0e42cf20e5957f5004aa6bd520ff
Jo2831 <jo2831@github.com>
2022-11-12 01:35:54 -0900

    updated_tasks6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,7 @@ def temp_n(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_or_even(n):
-    return render_template('6-number_odd_or_even.html')
+    return render_template('6-number_odd_or_even.html', value=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
