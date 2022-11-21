6939af8a204b87f9dd475a122b81d7a4db416535
Jo2831 <jo2831@github.com>
2022-11-21 05:44:25 -0900

    task10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python3
+"""
+Desc: This python module executes simple flask application
+"""
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filters_for_airbnb():
+    """Displays some data from db on the AirBnB website."""
+    states = storage.all("State")
+    amenity = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html", s=states, am=amenity)
+
+
+@app.teardown_appcontext
+def tear_down_db(execute):
+    """Removes the current SQLAlchemy session after each request
+    is completed"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
